28388c5b29263e6e68d18ec6af73c6bab481e3e9
cinhori <lilei93s@163.com>
2018-09-21 09:28:29 +0800

    添加开始界面

---
+++ app/src/main/java/com/dantou/adhoc/StartPage.java
@@ -0,0 +1,35 @@
+package com.dantou.adhoc;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class StartPage extends AppCompatActivity {
+    public final static long DELAY = 3 * 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_start_page);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        final Intent intent = new Intent(this, DeviceScanActivity.class);
+        Timer timer = new Timer();
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(intent);
+            }
+        };
+        timer.schedule(timerTask, DELAY);
+    }
+}
