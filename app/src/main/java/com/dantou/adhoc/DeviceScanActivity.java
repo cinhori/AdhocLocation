cf5de6103401ddd8fef99523c82e5bc650b134e2
cinhori <lilei93s@163.com>
2018-09-29 14:28:08 +0800

    设备界面点击返回时直接关闭设备

--- app/src/main/java/com/dantou/adhoc/DeviceScanActivity.java
+++ app/src/main/java/com/dantou/adhoc/DeviceScanActivity.java
@@ -30,6 +30,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -288,5 +289,22 @@ public class DeviceScanActivity extends ListActivity {
         } else {
 
         }
+    }
+
+    private long exitTime = 0;
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN){
+            if((System.currentTimeMillis()-exitTime) > 2000){
+                Toast.makeText(getApplicationContext(), "再按一次退出程序", Toast.LENGTH_SHORT).show();
+                exitTime = System.currentTimeMillis();
+            } else {
+                finish();
+                System.exit(0);
+            }
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
--- app/src/main/java/com/dantou/adhoc/StartPage.java
+++ app/src/main/java/com/dantou/adhoc/StartPage.java
@@ -2,9 +2,12 @@ package com.dantou.adhoc;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
+
+import com.baidu.mapapi.SDKInitializer;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -15,6 +18,7 @@ public class StartPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplicationContext());
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_start_page);
 
@@ -27,14 +31,14 @@ public class StartPage extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        final Intent intent = new Intent(StartPage.this, DeviceScanActivity.class);
-        Timer timer = new Timer();
-        TimerTask timerTask = new TimerTask() {
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                final Intent intent = new Intent(StartPage.this, DeviceScanActivity.class);
                 startActivity(intent);
+                finish();
             }
-        };
-        timer.schedule(timerTask, DELAY);
+        }, DELAY);
+
     }
 }
